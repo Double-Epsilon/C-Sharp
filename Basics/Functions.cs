5730315a0d35702ca15a6f57c095408943a1abd1
Epsilon <er2352387@gmail.com>
2026-05-18 19:55:28 -0600

    Modified Functions.cs

--- Basics/Functions.cs
+++ Basics/Functions.cs
@@ -27,36 +27,41 @@ string my_name = "Persona";
 saludar(my_name); // Llamar funcion con variable de parametro
 
 
-// No se we, una funcion que ordena cosillas =P
-// Ordenamiento "quicksort"
-List<int> quicksort (List<int> lista) { // Recibimos una lista como parametro
-    // list.Count : Retorna la cantidad de elementos en la lista 
-    if (lista.Count <= 1) { // Si solo hay 1 elemento, retornamos asi nomas
-        return lista;
-    }
+// --- Funciones Lambda ---------------------------------------
+// Para funciones que no retornan :
+// Action <tipos de dato parametros> nombre (parametros)
 
-    int pivote = lista[0]; // Pivote (comparador de si es menor o mayor)
-    List<int> izq = new List<int>(); // Lista izquierda (menores a 'pivote')
-    List<int> der = new List<int>(); // Lista derecha (mayores o iguales a 'pivote')
+// Para funciones que retornan :
+// Func <tipos de dato parametros, tipo de dato retorno> nombre (parametros)
 
-    // Iterar todos los elementos de la lista (excluyendo al pivote)
-    for (int i=1; i<lista.Count; i++) {
-        if (lista[i] < pivote) { // Si '[i]' es menor a 'pivote', agregamos a 'izq'
-            izq.Add(lista[i]);
-        } else { // Si '[i]' es mayor o igual a 'pivote', agregamos a 'der'
-            der.Add(lista[i]);
-        }
-    }
+// Funcion lambda que imprime algo (no retorna nada)
+Action <string> sayhi = (name) => {
+    Console.WriteLine($"Hi {name}!");
+};
 
-    List<int> first = quicksort(izq); // Ordenamos los elementos menores
-    List<int> mid = new List<int> {pivote}; // Mantenemos el pivote a la mitad
-    List<int> second = quicksort(der); // Ordenamos los elementos mayores
+// Funcion lambda que retorna la suma de 2 enteros
+Func <int, int, int> sum = (a, b) => a + b;
 
-    first.AddRange(mid); // Agregamos el pivote
-    first.AddRange(second); // Agregamos 'der' ya ordenado
-    return first; // Retornamos 'first' (menores ordenados + pivote + mayores ordenados)
+// Funcion lambda que retorna el cuadrado de un entero
+Func <int, int> sqrt = (a) => a * a;
+
+// Llamamos a las funciones lambda
+sayhi("stegosaurus");
+Console.WriteLine(sum(2, 3));
+Console.WriteLine(sqrt(6));
+
+
+// --- Uso chido de las lambda -------------------------
+// Puedes utilizarlas para pasar funciones como parametros
+
+// Llamamos a la funcion 'algo' con una funcion lambda como parametro
+// En este caso, la funcion lambda retorna 'a+b'
+algo((a, b) => a + b, 5);
+
+// Funcion que ocupa otra funcion como parametro
+void algo (Func <int, int, int> fn, int num) {
+    // Llamamos a la funcion 'fn' (su comportamiento esta establecido por la funcion parametro)
+    int res = fn(num, num);
+    // Imprimimos resultado
+    Console.WriteLine(res);
 }
-
-List<int> numeros = new List<int> {54, 29, 17, 88, 10, 1, 245};
-List<int> numsOrdenado = quicksort(numeros); // Llamar a 'quicksort' con 'numeros' de parametro
-Console.WriteLine(string.Join(", ", numsOrdenado)); // Imprimir lista ordenada
